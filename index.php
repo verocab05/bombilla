6f3f04cdd9b1dc157d2113babd7304f57b380fc8
verocab05 <veronica.cabello.rojo@gmail.com>
2019-06-14 15:36:42 +0200

    solucion basica

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<?php
+
+if( isset($_GET['on']) ) echo on();
+else echo off();
+
+function on(){
+	return '
+		<a href="index.php?off=1">
+			<img src="bombilla_on.jpg" style="height:200px" />
+		</a>
+	';
+}
+
+function off(){
+	return '
+		<a href="index.php?on=1">
+			<img src="bombilla_off.jpg" style="height:200px"/>
+		</a>
+	';
+}
+?>
