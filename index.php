4c6beb019e2cef13708cff8be3b52b5937520c3e
verocab05 <veronica.cabello.rojo@gmail.com>
2019-06-14 16:07:00 +0200

    solucion orientada a objetos

--- index.php
+++ index.php
@@ -1,23 +1,27 @@
 
 <?php
-// controlador
-if( isset($_GET['on']) ) echo on();
-else echo off();
+Class Bombilla{
+	//vista
+	function on(){
+		return '
+			<a href="index.php?off=1">
+				<img src="bombilla_on.jpg" style="height:200px" />
+			</a>
+		';
+	}
 
-//vista
-function on(){
-	return '
-		<a href="index.php?off=1">
-			<img src="bombilla_on.jpg" style="height:200px" />
-		</a>
-	';
+	function off(){
+		return '
+			<a href="index.php?on=1">
+				<img src="bombilla_off.jpg" style="height:200px"/>
+			</a>
+		';
+	}
 }
-
-function off(){
-	return '
-		<a href="index.php?on=1">
-			<img src="bombilla_off.jpg" style="height:200px"/>
-		</a>
-	';
-}
+	// controlador
+$b=new Bombilla();
+if( isset($_GET['on']) ) 
+	echo $b->on();
+else 
+	echo $b->off();
 ?>
